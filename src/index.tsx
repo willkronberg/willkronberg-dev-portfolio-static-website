2bb584f1022edda644d105e6111a2bf92b96e917
Will Kronberg <kron.dev@icloud.com>
2020-04-08 02:21:28 -0700

    Adding ESLint, Prettier, Redux, and Redux-Thunk Dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './redux';
+
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/modules/user.ts
@@ -0,0 +1,28 @@
+export function typedAction<T extends string>(type: T): { type: T };
+export function typedAction<T extends string, P extends any>(type: T, payload: P): { type: T; payload: P };
+
+export function typedAction(type: string, payload?: any) {
+  return { type, payload };
+}
+
+type UserState = {
+  username: string | null;
+};
+
+const initialState: UserState = { username: null };
+
+export const login = (username: string) => typedAction('user/LOGIN', username);
+export const logout = () => typedAction('user/LOGOUT');
+
+type UserAction = ReturnType<typeof login | typeof logout>;
+
+export function userReducer(state = initialState, action: UserAction): UserState {
+  switch (action.type) {
+    case 'user/LOGIN':
+      return { username: action.payload };
+    case 'user/LOGOUT':
+      return { username: null };
+    default:
+      return state;
+  }
+}
