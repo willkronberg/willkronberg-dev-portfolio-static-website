2761c584af8cd3dd171846b11de5d52d05b610e0
Will Kronberg <kronbew@LAPTOP-Q3UKOID5.localdomain>
2022-05-26 20:49:05 -0700

    Adds NPM Test Step to buildspec, Preps Unit Testing Suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
-test('renders hello world', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello World/i);
+const mockStore = configureStore([])
+
+test('renders the web page', () => {
+  const store = mockStore({
+    isDarkModeEnabled: false,
+  });
+
+  const { getByText } = render(<Provider store={store}><App preferences={{isDarkModeEnabled: false}} /></Provider>);
+  const linkElement = getByText(/willkronberg.dev/i);1
   expect(linkElement).toBeInTheDocument();
 });
